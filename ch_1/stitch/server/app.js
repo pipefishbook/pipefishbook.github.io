ce008fe809805b626dcfd8ddb89a6b60c661d722
Patrick Mulder <mulder.patrick@gmail.com>
2014-04-02 13:52:35 +0200

    Add stitch

--- ch_1/stitch/server/app.js
+++ ch_1/stitch/server/app.js
@@ -1,5 +1,7 @@
 // First, we require Express.js as dependency
-var express = require('express');
+var express = require('express'),
+    path = require('path'),
+    stitch = require('stitch');
 
 // a helper to resolve relative paths
 var path = require('path');
@@ -7,13 +9,24 @@ var path = require('path');
 // Then, we initialize the application...
 var app = express();
 
+var package = stitch.createPackage({
+  paths: [__dirname + '/../app'],
+  dependencies: [
+    __dirname + '/../libs/jquery.js',
+    __dirname + '/../libs/underscore.js',
+    __dirname + '/../libs/backbone.js',
+  ]
+});
+
+// Whenever a request goes to the client, we deliver the modules as client.js
+app.get('/static/bundle.js', package.createServer());
 
 // We add a basic route that serves an index.html
 // ... let's use the same as above
 app.get('/', function(req, res) {
-    console.log("--> /");
-    var html = path.resolve(__dirname + '/../index.html');
-    res.sendfile(html);
+  console.log("--> /");
+  var html = path.resolve(__dirname + '/../index.html');
+  res.sendfile(html);
 });
 
 // Let's listen on port 5000
