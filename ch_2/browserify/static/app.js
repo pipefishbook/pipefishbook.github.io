83c7e588cfca974d03d5af80e230111a3aabb113
Patrick Mulder <mulder.patrick@gmail.com>
2014-04-06 10:58:36 +0200

    select and unselect movies

--- ch_2/browserify/static/app.js
+++ ch_2/browserify/static/app.js
@@ -26,12 +26,27 @@ var Backbone = require('backbone');
   module.exports = Monitor;
 
 },{"backbone":7,"underscore":9}],4:[function(require,module,exports){
-  var Backbone = require('backbone');
-  var Movie = require('models/movie');
-  var Movies = Backbone.Collection.extend({
-    model: Movie 
-  });
-  module.exports = Movies;
+    var Backbone = require('backbone');
+    var Movie = require('models/movie');
+    var Movies = Backbone.Collection.extend({
+      model: Movie,
+
+      // Unselect all models
+      resetSelected: function() {
+        this.each(function(model) {
+          model.set({"selected": false}, {silent: true});
+        });
+      },
+
+      // Select a specific model from the collection
+      selectByID: function(id) {
+        this.resetSelected();
+        var movie = this.get(id);
+        movie.set({"selected": true});
+        return movie.id;
+      }
+    })
+    module.exports = Movies;
 
 },{"backbone":7,"models/movie":5}],5:[function(require,module,exports){
 var Backbone = require("backbone");
